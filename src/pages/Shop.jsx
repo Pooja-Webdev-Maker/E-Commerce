9143d34af7fcdfa5a7e7e137e7a15c0da43d0dba
Pooja yadav <poojayadavay28@gmail.com>
2025-08-17 17:51:17 +0530

    completed Home Page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
+import Offers from '../components/Offers/Offers'
+import NewCollections from '../components/NewCollections/NewCollections'
+import NewsLetter from '../components/NewsLetter/NewsLetter'
 function Shop() {
   return (
-    <div>Shop</div>
+    <div>
+     <Hero />
+     <Popular />
+     <Offers />
+     <NewCollections />
+     <NewsLetter />
+    </div>
+   
   )
 }
 
