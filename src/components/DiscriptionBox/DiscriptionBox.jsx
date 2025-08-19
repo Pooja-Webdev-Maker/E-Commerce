7d1e2b8f19c17512ff9a5d0567ce0c3f2dcc8c81
Pooja yadav <poojayadavay28@gmail.com>
2025-08-19 18:05:04 +0530

    complete the product page

---
+++ src/components/DiscriptionBox/DiscriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DiscriptionBox.css'
+
+function DiscriptionBox() {
+  return (
+    <div className='discriptionbox'>
+       <div className="discriptionbox-navigator">
+        <div className="discriptionbox-nav-box">Description</div>
+        <div className="discriptionbox-nav-box fade">Reviews(122)</div>
+       </div>
+       <div className="discriptionbox-discription">
+        <p>E-commerce, or electronic commerce, is the buying and selling of goods and services, or the transmission of funds or data, over an electronic network, primarily the internet. It encompasses all online activities related to the purchase and sale of products and services. This includes transactions between businesses (B2B). </p>
+        <p>E-commerce, or electronic commerce, is the buying and selling of goods and services, or the transmission of funds or data, over an electronic network, primarily the internet. It encompasses all online activities related to the purchase and sale of products and services. This includes transactions between businesses (B2B). </p>
+       </div>
+    </div>
+  )
+}
+
+export default DiscriptionBox
---
+++ src/pages/Product.jsx
@@ -0,0 +1,22 @@
+import React,{useContext}from 'react'
+import { ShopContext } from '../context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../components/Breadcrum/Breadcrum';
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
+import DiscriptionBox from '../components/DiscriptionBox/DiscriptionBox';
+function Product() {
+    const {all_product} = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_product.find((e)=> e.id === Number(productId));
+
+
+  return (
+    <div>
+        <Breadcrum product = {product}/>
+        <ProductDisplay product={product} />
+        <DiscriptionBox/>
+    </div>
+  )
+}
+
+export default Product
